ed1e592286e00e0c7bd4c8882e2c21cb53e2896b
Radu Vadim <raduvadim@gmail.com>
2015-03-18 23:11:06 +0200

    code-update

--- software/lcd_char.h
+++ software/lcd_char.h
@@ -128,7 +128,7 @@
 #if LCD_IO_MODE
 
 #ifndef LCD_PORT
-#define LCD_PORT         PORTA        /**< port for the LCD lines   */
+#define LCD_PORT         PORTB        /**< port for the LCD lines   */
 #endif
 #ifndef LCD_DATA0_PORT
 #define LCD_DATA0_PORT   LCD_PORT     /**< port for 4bit data bit 0 */
@@ -146,7 +146,7 @@
 #define LCD_DATA0_PIN    0            /**< pin for 4bit data bit 0  */
 #endif
 #ifndef LCD_DATA1_PIN
-#define LCD_DATA1_PIN    1            /**< pin for 4bit data bit 1  */
+#define LCD_DATA1_PIN    7            /**< pin for 4bit data bit 1  */
 #endif
 #ifndef LCD_DATA2_PIN
 #define LCD_DATA2_PIN    2            /**< pin for 4bit data bit 2  */
--- software/main.c
+++ software/main.c
@@ -18,18 +18,31 @@
 int main(void)
 {
     /*defs*/
-    
+    uint8_t pwr= 0;
     /* initialize display, cursor off */
     lcd_init(LCD_DISP_ON);
-    lcd_clrscr();
     lcd_home();
-    lcd_puts("SolSt starting...");
+    lcd_puts("SolSt starting..");
     /*periphereal init*/
-    
+    //pwm init
+	TCCR1B |= (1<<CS11)|(1<<CS10);
+    TCCR1A |= (1<<COM1A1)|(1<<WGM11);
+    OCR1A = 0;
+    DDRB |= (1<<PB1);
+	//button init
+	#define LED_MASK1 (1<<PC0)
+	#define LED_MASK2 (1<<PC1)
+	#define LED_PORT PORTC
+	#define LED_DDR DDRC
+	
+	lcd_gotoxy(0,1);
     lcd_puts("ok.");
-    //a delay should be inserted
+	_delay_ms(1000);
+    lcd_clrscr();
+	lcd_home();    
+	lcd_puts("Power:");
     while (1) {  
     /* loop forever */
-    //should create menu
-    }
+		
+	}
 }
