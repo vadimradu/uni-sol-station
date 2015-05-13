5ae7ab3653ca8fea65f8d0ff948bd2d2798cbba7
Radu Vadim <raduvadim@gmail.com>
2015-05-13 13:22:51 +0300

    Update main.c

--- software/main.c
+++ software/main.c
@@ -6,11 +6,11 @@
  * Controlul temperaturii se face cu un algoritm PID, cu parametrii modificabili
  *
  * Control serial: 	solst set <parameter> <value>	-parametrii configurabili (KP KI KD KT)
- *	    			solst log <0/1>			-in scopuri de debug sau urmarirea comportamentului
- *					solst config			-afiseaza parametrii setati
- *					solst eeprom		    -afiseaza valorile salvate in eeprom
- *					solst save				-salveaza noii parametrii in memorie
- *					solst temp				-seteaza temperatura dorita
+ *	    			solst log <0/1>		-in scopuri de debug sau urmarirea comportamentului
+ *				solst config		-afiseaza parametrii setati
+ *				solst eeprom		-afiseaza valorile salvate in eeprom
+ *				solst save		-salveaza noii parametrii in memorie
+ *				solst temp		-seteaza temperatura dorita
  *
  * Nota: Programul actual este construit pentru un ATmega8, dar poate rula pe orice procesor AVR cu cel putin 2 timere, PWM,
  *	interfata seriala si, optional, pini pentru butoane si afisaj.
@@ -32,7 +32,7 @@ int main(void)
     /*periphereal init*/
     cli();
     /* initialize display, cursor off */
-    lcd_init(LCD_DISP_ON);    lcd_clrscr();    lcd_home();
+    lcd_init(LCD_DISP_ON);    lcd_clrscr();    lcd_home();	lcd_puts("SolSt starting...");
     /* intitialize timers, as follows: timer0-button_input/menu_display, timer1-heater pwm, timer2-temp reading */
     TIMER0_init(); //isr takes care of menu display
     TIMER1_init(); //pwm generator for heater
@@ -55,75 +55,12 @@ int main(void)
     sei();
     
     // Be polite and introduce itself :-)
-	//should be completed with some lcd printing as well
 	printf_P(STATUS, pid_s.KP, pid_s.KI, pid_s.KD, pid_s.KT);
 	printf_P(OK);
+	lcd_puts("ok.");
 	
     while (1) {  
-    	if (fgets(line,LINE_SIZE-1,stdin)){
-			if(strncmp_P(line, PSTR("solst"), 5) == 0){
-				if (strncmp_P(line,PSTR("set KP "),7) == 0){ //set KP n
-					sscanf_P(line,PSTR("set KP %d"),&tmp); 
-
-					if (tmp < MAX_PID_CONSTANT && tmp >= 0){
-						pid_s.KP = (uint8_t)tmp;	printf_P(OK);
-					}else{
-						printf_P(PARAM_RANGE_MSG,MAX_PID_CONSTANT);
-					}
-				}else if (strncmp_P(line,PSTR("set KI "),7) == 0){ //set KI n
-					sscanf_P(line,PSTR("set KI %d"),&tmp);
-
-					if (tmp < MAX_PID_CONSTANT && tmp >= 0){
-						pid_s.KI = (uint8_t)tmp;	printf_P(OK);
-					}else{
-						printf_P(PARAM_RANGE_MSG,MAX_PID_CONSTANT);
-					}
-				}else if (strncmp_P(line,PSTR("set KD "),7) == 0){ //set KD n
-					sscanf_P(line,PSTR("set KD %d"),&tmp);
-
-					if (tmp < MAX_PID_CONSTANT && tmp >= 0){
-						pid_s.KD = (uint8_t)tmp;	printf_P(OK);
-					}else{
-						printf_P(PARAM_RANGE_MSG,MAX_PID_CONSTANT);
-					}
-				}else if (strncmp_P(line,PSTR("set KT "),7) == 0){ //set KT n
-					sscanf_P(line,PSTR("set KT %d"),&tmp);
-
-					if (tmp < 255 && tmp >= 0){
-						pid_s.KT = (uint8_t)tmp;	printf_P(OK);
-					}else{
-						printf_P(PARAM_RANGE_MSG,255);
-					}
-				}else if (strncmp_P(line,PSTR("log "),3) == 0){ // log 0/1
-					sscanf_P(line,PSTR("log %d"),&tmp);
-
-					if (tmp == 0 || tmp == 1)	log_enable = (uint8_t)tmp;
-					if (log_enable)				printf_P(PSTR("setpoint\ttemperature\tpower\n"));
-					else						printf_P(OK);
-				}else if (strncmp_P(line,PSTR("save"),4) == 0){
-					cli();
-					eeprom_busy_wait();
-					eeprom_write_byte(&EEMEM_KP, pid_s.KP);
-					eeprom_write_byte(&EEMEM_KI, pid_s.KI);
-					eeprom_write_byte(&EEMEM_KD, pid_s.KD);
-					eeprom_write_byte(&EEMEM_KT, pid_s.KT);
-					sei();
-					printf_P(OK);
-				}else if (strncmp_P(line,PSTR("config"),6) == 0){
-					printf_P(STATUS,pid_s.KP,pid_s.KI,pid_s.KD,pid_s.KT);
-					printf_P(OK);
-				}else if (strncmp_P(line,PSTR("eeprom"),6) == 0){
-					printf_P(STATUS, \
-						eeprom_read_byte(&EEMEM_KP), \
-						eeprom_read_byte(&EEMEM_KI), \
-						eeprom_read_byte(&EEMEM_KD), \
-						eeprom_read_byte(&EEMEM_KT));
-					printf_P(OK);
-				}else{
-					printf_P(PSTR("?\n>"));
-				}
-			}
-		}
+    	usart_comm();
     }
     return 0;
 }
