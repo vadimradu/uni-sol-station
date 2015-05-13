9f7ea8bb625948c32b7f229d34d442eb1ab4fa91
Radu Vadim <raduvadim@gmail.com>
2015-05-13 12:42:27 +0300

    Update main.c

--- software/main.c
+++ software/main.c
@@ -1,18 +1,26 @@
+/* fisier: main.c
+ * 12/07/2014 vadim@anti
+ * Bazat pe codul original de la: https://www.openhub.net/p/avr-pid-solder-station?ref=sample
+ * Controllerul pentru statia de lipit se foloseste de doua interfete pentru comenzi.
+ * Prima este interfata seriala(baud:9600 start8stop) si trei butoane (- + ok)
+ * Controlul temperaturii se face cu un algoritm PID, cu parametrii modificabili
+ *
+ * Control serial: 	solst set <parameter> <value>	-parametrii configurabili (KP KI KD KT)
+ *	    			solst log <0/1>					-in scopuri de debug sau urmarirea comportamentului
+ *					solst config					-afiseaza parametrii setati
+ *					solst eeprom					-afiseaza valorile salvate in eeprom
+ *					solst save						-salveaza noii parametrii in memorie
+ *					solst temp						-seteaza temperatura dorita
+ *
+ * Nota: Programul actual este construit pentru un ATmega8, dar poate rula pe orice procesor AVR cu cel putin 2 timere, PWM,
+ *	interfata seriala si, optional, pini pentru butoane si afisaj.
+*/
+
 #include <stdlib.h>
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 #include "lcd_char.h"
-
-
-/*
-** constant definitions
-*/
-
-
-/*
-** function prototypes
-*/ 
 
 
 int main(void)
