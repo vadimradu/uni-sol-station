99d31f4f7634dcf7d99565616f12439766df9c76
Radu Vadim <raduvadim@gmail.com>
2015-05-13 13:35:42 +0300

    Update hardware.h

--- software/hardware.h
+++ software/hardware.h
@@ -8,25 +8,24 @@
 #define HARDWARE_H_
 
 // SPI			ATmega8	ATmega16	
-#define SPI_PORT 		PORTB
+#define SPI_PORT 		  PORTB
 #define SPI_PORT_DIR 	DDRB
-#define SPI_SCK 		PB5
-#define SPI_MISO 		PB4
-#define SPI_MOSI 		PB3
-#define SPI_CS			PB2
+#define SPI_SCK 		  PB5
+#define SPI_MISO 		  PB4
+#define SPI_MOSI 		  PB3
+#define SPI_CS		  	PB2
 
 // USART
 #define USART_U2X
 #define BAUD 9600UL
 
-// afisaj(4x7seg)
-//#define DIGIT_PORT PORTC
-//#define DIGIT_PORT_DIR DDRC
-//#define CATHODE_PORT PORTB
-//#define CATHODE_PORT_DIR DDRB
+//LCD port declares are in lcd_char.h
 
 //Butoane input
-//#define BUTTON_PORT PORTC
-//#define BUTTON_PORT_DIR DDRC
+#define BUTTON_PORT     PORTC
+#define BUTTON_PORT_DIR DDRC
+#define MINUS_BUTTON    PC0
+#define PLUS_BUTTON     PC1
+#define OK_BUTTON       PC2
 
 #endif /* HARDWARE_H_ */
