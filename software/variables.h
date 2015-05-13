97f5017ed893f5b83eab12c219b98d2fbf0929c6
Radu Vadim <raduvadim@gmail.com>
2015-05-13 14:48:44 +0300

    Update variables.h

--- software/variables.h
+++ software/variables.h
@@ -23,4 +23,13 @@ volatile int16_t temperature = 0;
 int16_t tmp;
 uint8_t button=0;
 uint8_t button_activity=0;
+uint8_t submenu_active =0;
+uint8_t crt_menu = 0;
 
+const char menu_header[3][14] PROGMEM = {	"Oreview", "Amb. temp.", "Other"};
+const char menu_content[3][16] PROGMEM = {"Set:    Crt:    ","    'C","c Radu Vadim"};
+
+const char *OK = PSTR("OK\n>");
+const char *PARAM_RANGE_MSG = PSTR("Out of range 0-%d\n>");
+const char *STATUS = PSTR("SolSt\n\tKP %d\n\tKI %d\n\tKD %d\n\tKT %d\n");
+char line[LINE_SIZE]; //line buffer
