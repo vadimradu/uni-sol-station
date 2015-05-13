fc4e8d4a76d0a738cd816d93aa7c404d21fc90c5
Radu Vadim <raduvadim@gmail.com>
2015-05-13 13:28:04 +0300

    Create spi.h

---
+++ software/spi.h
@@ -0,0 +1,14 @@
+/*
+ * spi.h
+ *
+ *  Created on: 2008-11-27
+ *      Author: wodz
+ */
+
+#ifndef SPI_H_
+#define SPI_H_
+
+void spi_byte_send (uint8_t byte);
+uint8_t spi_byte_receive (void);
+
+#endif /* SPI_H_ */
